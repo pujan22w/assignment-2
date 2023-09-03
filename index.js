13a8858517ab5f531f074a7153d8cce4291f1297
pujan22 <puzankoirala786@gmail.com>
2023-09-03 22:24:27 +0545

    uploaded all

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let number1= +prompt("Enter a number you want to do square");
 
 for(i=0;i<number1;i++){
  let value= parseFloat(prompt(`Enter number is ${i+1}: `))
- console.log(value);
+ console.log(`num${i} is :${value}`);
  
 function loop(number){
     let num= number*number;
-    console.log(num);
+    console.log(`Square of ${value} is  ${num}`);
     alert(`Square of ${value} is :${num}`);
 }
 loop(value);
